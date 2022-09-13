e689892338be93bf045f2d37d9479e9546b1722b
cheng2 <cheng2@allegheny.edu>
2022-09-13 19:00:36 -0400

    code: providing code for race

---
+++ src/code.py
@@ -0,0 +1,21 @@
+# testing code here 
+
+# logic 
+
+from itertools import dropwhile
+
+import easygopigo3 as easy
+
+my_gopigo = easy.EasyGoPiGo3()
+
+my_gopigo.drive_inches(119)
+my_gopigo.turn_degrees(180)
+my_gopigo.drive_inches(119)
+
+# install cam 
+# find a way for the camera to follow an item we drop
+# find a way to bring it back to us
+# push to the user/owner
+# install a hand to grab item 
+# hold it in place? or lift? yes lift
+
