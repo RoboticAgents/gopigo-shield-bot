e3450edb19d6224076f47ac0e59f682909f9cd9c
HoangTrang150302 <hoangtrang150302@gmail.com>
2022-09-22 22:49:09 -0400

    add avoid object code

---
+++ src/avoid_object.py
@@ -0,0 +1,28 @@
+import easygopigo3 as easy
+import time
+import pygame
+
+gpg = easy.EasyGoPiGo3()
+my_distance_sensor = gpg.init_distance_sensor() # mm
+max_dis_t = 25
+min_distance = 250 # in (mm)
+t = 0  
+pygame.mixer.init()    
+pygame.mixer.music.load("music.mp3")
+mixer.music.set_volume(1.0)
+pygame.mixer.music.play()
+     
+while t <= max_dis_t:
+    # Directly print the values of the sensor.
+    print("Distance Sensor Reading (mm): " + str(my_distance_sensor.read_mm()))
+    
+    t = t + 1
+    print("time = ", t)
+    
+    # if not encouter an object
+    if my_distance_sensor.read_mm() > min_distance:
+        gpg.drive_inches(5)       
+    else: 
+        gpg.turn_degrees(90) # turn right       
+
+mixer.music.stop()
